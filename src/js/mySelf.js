1c6dca372289ddea029880792cc881ed01fc8ddd
周军 <zhoujun@zhoujundeMacBook-Pro.local>
2016-04-12 22:13:25 +0800

    add canvasJS

--- src/js/mySelf.js
+++ src/js/mySelf.js
@@ -4,4 +4,9 @@
 $('body').css({background:'#090'});
 //canvas c1
 $('#c1').css({width:750,height:200,background:'yellow'});
-//var gc=$('#c1').getContext('2d');
+var oc1=document.querySelector('#c1');
+var gc1=oc1.getContext('2d');
+gc1.moveTo(20,20);
+gc1.lineTo(100,100);
+gc1.strokeStyle='red';
+gc1.stroke();
