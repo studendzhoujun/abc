5a696a6455e6d5e2df3e008d8c041edcc8314d57
周军 <zhoujun@zhoujundeMacBook-Pro.local>
2016-04-12 22:03:21 +0800

    modified mySelf.js

--- src/js/mySelf.js
+++ src/js/mySelf.js
@@ -3,4 +3,5 @@
 //date   2016/04/12
 $('body').css({background:'#090'});
 //canvas c1
-var gc=$('#c1').getContext('2d');
+$('#c1').css({width:750,height:200,background:'yellow'});
+//var gc=$('#c1').getContext('2d');
