62678588ff1afd9bba0f927aa8d171e34255a9f5
周军-前端业务开发部-前端工程师 <zhoujun@gomeplus.com>
2016-04-29 10:18:42 +0800

    mySelf

--- src/js/mySelf.js
+++ src/js/mySelf.js
@@ -6,11 +6,13 @@ $('body').css({background:'#090'});
 //canvas c1
 
 //canvas c1
-$('#c1').css({width:750,height:200,background:'yellow'});
+$('#c1').css({width:750,height:350,background:'yellow'});
 var oc1=document.querySelector('#c1');
 var gc1=oc1.getContext('2d');
-gc1.moveTo(20,20);
+/*gc1.moveTo(20,20);
 gc1.lineTo(100,100);
 gc1.strokeStyle='red';
+gc1.stroke();*/
+gc1.strokeStyle ='red';
+gc1.arc(60, 60, 20, 0, 1.5*Math.PI, false);
 gc1.stroke();
-
