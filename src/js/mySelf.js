e2ec14d66a4f086204b3eecb18040590d7e96bfc
周军-前端业务开发部-前端工程师 <zhoujun@gomeplus.com>
2016-04-29 10:22:29 +0800

    solve conflict

--- src/js/mySelf.js
+++ src/js/mySelf.js
@@ -2,13 +2,6 @@
 //author zhoujun
 //date   2016/04/12
 $('body').css({background:'#090'});
-<<<<<<< HEAD
-
-//canvas c1
-
-=======
-//canvas c1
->>>>>>> 1ba7cee235a8221949a29c34030077bccd94661a
 //canvas c1
 $('#c1').css({width:750,height:350,background:'yellow'});
 var oc1=document.querySelector('#c1');
