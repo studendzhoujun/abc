4fc171afe6327a47936a792ecea2d99a94c758e9
周军-技术中心-前端业务开发部 <zhoujun@gomeplus.com>
2016-05-20 15:04:46 +0800

    mod st.js

--- src/js/st.js
+++ src/js/st.js
@@ -50,7 +50,7 @@ define('conf/coquetry/st',function(require,exports,module){
         }); 
      	},
      	console:function(str){
-     		  var str=str||'studentzhoujun';
+     		  var str=str||'gomeplus';
              console.log('%c'+str, 'background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );color:transparent;-webkit-background-clip: text;font-size:2em;');
      	},
      	rnd:function(n,m){
