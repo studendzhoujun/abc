05506c4f9e654340096099c76a1b5410b8834f96
zhoujun <studentzhoujun@sina.com>
2017-08-08 22:42:06 +0800

    @

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
-
+import App from '@/App'
+import router from '@/router'
+import config from '@/config.js'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -13,3 +13,4 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+console.log(config)
